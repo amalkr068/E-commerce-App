f1559403bc2cb568d7bd28f18560cc528fda3666
amalkr068 <amalkr068@gmail.com>
2024-12-21 18:58:02 -0800

    Customer Management, listing userdata in Admin side, block and unblock users

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,16 +1,20 @@
 const express = require("express")
 const router = express.Router()
 const { loadLogin,login,loadDashBoard,pageerror,logout } = require("../controllers/admin/adminController")
-const 
+const { customerInfo,customerBlocked,customerUnblocked } = require("../controllers/admin/customerController")
 const { adminAuth } = require("../middlewares/auth")
 
-
+// Log in Management
 router.get("/login",loadLogin)
 router.post("/login",login)
 router.get("/",adminAuth,loadDashBoard)
-router.get("/pageerror",pageerror)
 router.get("/logout",logout)
 
+// Customer Management
+router.get("/users", customerInfo)
+router.get("/blockCustomer", customerBlocked)
+router.get("/unblockCustomer", customerUnblocked)
 
+router.get("/pageerror",pageerror)
 
 module.exports = router;
