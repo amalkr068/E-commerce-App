9d3363202c5999b824c9ab9a94f0c881c0afcc16
amalkr068 <amalkr068@gmail.com>
2024-12-24 22:45:59 -0800

    Product Adding feature done

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,7 +4,7 @@ const { loadLogin,login,loadDashBoard,pageerror,logout } = require("../controlle
 const { customerInfo,customerBlocked,customerUnblocked } = require("../controllers/admin/customerController")
 const { categoryInfo,addCategory,addCategoryOffer,removeCategoryOffer,getListCategory,getUnListCategory,getEditCategory,editCategory } =  require("../controllers/admin/categoryController")
 const { getBrandPage,addBrand,blockBrand,unBlockBrand,deleteBrand } = require("../controllers/admin/brandController")
-const { getProductAddPage} = require("../controllers/admin/productController")
+const { getProductAddPage,addProducts } = require("../controllers/admin/productController")
 const { adminAuth } = require("../middlewares/auth")
 const multer = require("multer")
 const storage = require("../helpers/multer")
@@ -46,6 +46,7 @@ router.get("/deleteBrand",deleteBrand)
 
 //Product Management
 router.get("/addProducts",getProductAddPage)
+router.post("/addProducts",uploads.array("images",4),addProducts)
 
 router.get("/pageerror",pageerror)
 
