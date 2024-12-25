7d8010e26355182724795f537091536c7a5d3b3c
amalkr068 <amalkr068@gmail.com>
2024-12-25 02:36:16 -0800

    block/unblock Products

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -162,8 +162,29 @@ const removeProductOffer = async (req,res)=>{
 }
 
 
+const blockProduct = async(req,res)=>{
+    try {
+        let id = req.query.id
+        await Product.updateOne({_id:id},{$set:{isBlocked:true}})
+        res.redirect("/admin/products")
+    } catch (error) {
+        res.redirect("/pageerror")
+    }
+}
 
-module.exports = { getProductAddPage,addProducts,getAllProducts,addProductOffer,removeProductOffer }
+
+const unblockProduct = async(req,res)=>{
+    try {
+        let id = req.query.id
+        await Product.updateOne({_id:id},{$set:{isBlocked:false}})
+        res.redirect("/admin/products")
+    } catch (error) {
+        res.redirect("/pageerror")
+    }
+}
+
+
+module.exports = { getProductAddPage,addProducts,getAllProducts,addProductOffer,removeProductOffer,blockProduct,unblockProduct }
 
 
 
